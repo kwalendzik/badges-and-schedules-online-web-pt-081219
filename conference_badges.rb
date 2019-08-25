c6a61591a3853db96b725655e209f99179e4f89d
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-25 02:05:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,20 +2,24 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-batch_badge_creator(attendees)
+def batch_badge_creator(attendees)
   attendees.map do |name|
     badge_maker(name)
   end
 end
 
+def assign_room(name, number)
+  "Hello, #{name}! You'll be assigned to room #{number}!"
+end
+
 def assign_rooms(attendees)
+  counter = 1
   result = []
-  index = 0
-  index += 1
   attendees.each do |name|
-  "Hello, #{name}! You'll bee assigned to room #{counter}!"
+    result << assign_room(name, counter)
+    counter += 1
   end
-result
+  result
 end
 
 def printer(attendees)
